d31e39d7f8336ee84baf2f1fb62e31b69d6736a6
Bernardo Henrique Lander Leefmans <bernardo.lander@farmatodo.com>
2025-09-05 20:33:45 +0000

    fix the login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,25 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/auth-context";
 
 export default function Home() {
   const router = useRouter();
+  const { currentUser, loading } = useAuth();
 
   useEffect(() => {
-    router.replace("/login");
-  }, [router]);
+    if (!loading) {
+      if (currentUser) {
+        router.replace("/app");
+      } else {
+        router.replace("/login");
+      }
+    }
+  }, [router, currentUser, loading]);
 
-  return null;
+  return (
+     <div className="flex h-screen items-center justify-center">
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+      </div>
+  );
 }
